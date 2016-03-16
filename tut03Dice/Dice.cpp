561e60b6ec6b13799a2b8072b5bce3c2425e0f6d
BushK <kmabusha2@GMAIL.COM>
2016-03-16 11:30:35 +0200

    header file

---
+++ tut03Dice/Dice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+class Dice
+{
+public:
+	Dice();
+	int roll();
+	float average(Dice, int);
+	float average(int[], int);
+	~Dice();
+
+
+private:
+
+};
---
+++ tut03Dice/Dice.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class Dice
+{
+public:
+	Dice();
+	int roll();
+	float average(Dice, int);
+	float average(int[], int);
+	~Dice();
+P
+
+};
+
